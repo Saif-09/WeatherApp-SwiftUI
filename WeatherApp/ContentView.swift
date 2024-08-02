571ae63da0d71e6f325c006ce9eb0ab595cff089
Saif-09 <saifmd238@gmail.com>
2024-08-02 21:15:50 +0530

    initialized

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(
+                gradient: Gradient(colors: [Color.blue, Color.white]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Jaipur, India")
+                    .font(
+                        .system(size: 32,weight: .medium, design: .default))
+                    .foregroundStyle(.white)
+                    .padding()
+//                    .frame(width:200, height: 200)
+//                    .background(Color.red )
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
